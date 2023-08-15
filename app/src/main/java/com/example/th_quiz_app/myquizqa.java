2a895844645d372591a6e27b79bf542fb17dd7e2
Sriram Hegde <sriramhegde44335@gmail.com>
2023-08-15 21:54:05 +0530

    Initial commit

---
+++ app/src/main/java/com/example/th_quiz_app/myquizqa.java
@@ -0,0 +1,49 @@
+package com.example.th_quiz_app;
+
+public class myquizqa {
+
+
+
+    public static String question[] ={
+
+            "What is the pH value of the human body??",
+            "Which is the largest ocean in the world?",
+            "Elections to panchayats in state are regulated by?",
+            "Which bank is called bankers Bank of India?",
+            "Digestion of food in human beings begins in which part of the alimentary canal?",
+            "The driving force of an ecosystem is?",
+            "Which company owns the android?",
+            "Which one is not the programming language?",
+            "Which of the given is a disease caused by protozoa?",
+            "Tsunami is a word in which language?"
+
+
+    };
+
+    public static String choices[][] = {
+            {"9.2 to 9.8","7.0 to 7.8","6.1 to 6.3", "5.4 to 5.6"},
+            {"Indian Ocean" ,"Pacific Ocean" , "Arctic Ocean", "Atlantic Ocean"},
+            {"Gram panchayat" ,"Nagar Nigam" , "Election Commission of India" , "State Election Commission"},
+            {"Reserve Bank of India" ,"Punjab National Bank" ,"State Bank of India","ICICI Bank"},
+            {"Mouth","Large intestine","Liver", "Mouth"},
+            {"Carbon Mono oxide","Biogas","Solar Energy","Carbon dioxide"},
+            {"Google","Apple","Nokia","Samsung"},
+            {"Java","Kotlin","Notepad","Python"},
+            {"Cance","Typhoid","Kala-azar", "Chicken Pox"},
+            {"Hindi","Urdu","Japanese", "French"}
+    };
+
+    public static String correctAnswer[] = {
+            "7.0 to 7.8",
+            "Pacific Ocean",
+            "State Election Commission",
+            "Reserve Bank of India",
+            "Mouth",
+            "Solar Energy",
+            "Google",
+            "Notepad",
+            "Kala-azar",
+            "Japanese"
+    };
+
+}
